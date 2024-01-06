002b3155912d86fda55720da4e1017fbc9569518
Irina Aynes Balagué <irina.aynes@salle.url.edu>
2024-01-06 12:22:10 +0100

    commit prova irina

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demofffdfd',
+      title: 'Flutter Demo provaaa',
       theme: ThemeData(
         // This is the theme of your application.
         //
